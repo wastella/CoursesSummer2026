d0864b5f91340ade3cfb9a10692aef8cb8d0e0ad
wastella <william.a.stella@gmail.com>
2026-07-15 19:19:39 -0400

    first part of project 3

--- StanfordCS336/assignment1-basics/cs336_basics/bpe_tokenizer.py
+++ StanfordCS336/assignment1-basics/cs336_basics/bpe_tokenizer.py
@@ -12,73 +12,62 @@ def BPETokenizer(input_path, vocab_size):
 
     # Initialize vocab with first 256 unicode chars
     for i in range(256):
-        vocab.update({i: bytes([i])})
+        vocab.update({bytes([i]): i})
 
-    input_text = ""
-    with open(input_path) as f:
-        input_text += f.read()
+    input_text = "low low low low low lower lower widest widest widest newest newest newest newest newest newest"
+    #with open(input_path) as f:
+        #input_text += f.read()
 
     # Pre-tokenization
     PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 
     pretok_freq_table = {}
     for pre_tok in re.finditer(PAT, input_text):
-        if pre_tok in pretok_freq_table:
-            pretok_freq_table[tuple(pre_tok.group())] += 1
+        token = tuple(pre_tok.group())
+        if token in pretok_freq_table:
+            pretok_freq_table[token] += 1
         else:
-            pretok_freq_table[tuple(pre_tok.group())] = 1
+            pretok_freq_table[token] = 1
 
-    while len(vocab) != vocab_size:
-        # Get all pair
-        """
-        print("Before:")
-        for k, v in pretok_freq_table.items():
-            print(k, end="")
-            print(" : " + str(v) + "\n")
-        """
+    #for itttter in range(1):
         # Compute BPE merges
-        freq = {}
-        for pretok in pretok_freq_table:
-            pretok_freq = pretok_freq_table[pretok]
-            for i in range(0, len(pretok) - 1):
+    freq = {}
+    for word in pretok_freq_table:
+        length = len(word)
+        if length > 1:
+            for i in range(0, length - 1):
                 # Get pair i and i+1
-                curr = pretok[i]
-                next = pretok[i + 1]
+                curr = word[i]
+                next = word[i + 1]
                 pair = (curr, next)
                 if pair in freq:
-                    freq[pair] += pretok_freq
+                    freq[pair] += pretok_freq_table[word]
                 else:
-                    freq[pair] = pretok_freq
+                    freq[pair] = pretok_freq_table[word]
 
-        sorted_freq = sorted(freq.keys(), key=lambda k: freq[k], reverse=True)
-        most_freq = sorted_freq[0]
-        # print(most_freq)
-        for key in list(pretok_freq_table.keys()):
-            new_key = []
-            for i in range(0, len(key)):
-                # print(str(i) + " : " + key[i])
-                # print(new_key)
-                if i < len(key) - 1 and key[i] == most_freq[0] and key[i + 1] == most_freq[1]:
-                    new_key.append(str(most_freq[0] + most_freq[1]))
-                elif i != 0 and key[i] == most_freq[1] and key[i - 1] == most_freq[0]:
-                    continue
-                else:
-                    new_key.append(key[i])
+    winner = sorted(freq.keys(), key=lambda k: freq[k], reverse=True)[0]
 
-            pretok_freq_table[tuple(new_key)] = pretok_freq_table.pop(key)
-            vocab[str(most_freq[0] + most_freq[1])] = len(vocab)
+    print(winner[0])
+    print(winner[1])
+    for key in list(pretok_freq_table.keys()):
+        new_key = []
+        i = 0
+        while i < len(key):
+            if i < len(key)-1 and key[i] == winner[0] and key[i + 1] == winner[1]:
+                new_key.append(str(winner[0] + winner[1]))
+                i += 2
+            else:
+                new_key.append(key[i])
+                i += 1
 
-        """
-        print("After running: ")
-        for k, v in pretok_freq_table.items():
-            print(k, end="")
-            print(" : " + str(v) + "\n")
-        """
+        pretok_freq_table[tuple(new_key)] = pretok_freq_table.pop(key)
+        vocab[len(vocab)] = str(winner[0] + winner[1])
+
+    print(pretok_freq_table)
     return vocab
 
 
 dict = BPETokenizer("test.txt", 500)
-print(dict)
 """
 for k, v in dict.items():
     print(k, end="")
